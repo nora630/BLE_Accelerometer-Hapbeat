56807290a335d62acbe77430cfe82b7803aad532
Onodera Sho <onodera@haselab.net>
2021-08-03 02:04:56 +0900

    add something

---
+++ examples/ble_peripheral/ble_app_template_hapbeat/pca10040/s132/ses/Output/ble_app_template_pca10040_s132 Release/Obj/ses_startup_nrf52.d
@@ -0,0 +1,2 @@
+Output/ble_app_template_pca10040_s132\ Release/Obj/ses_startup_nrf52.o: \
+ C:\Users\sho_o\labo\Nordic52SDK\nRF5SDK1702\nRF5_SDK_17.0.2\modules\nrfx\mdk\ses_startup_nrf52.s
---
+++ examples/ble_peripheral/ble_app_template_two_accelerometer/pca10056/s140/ses/Output/ble_app_template_pca10056_s140 Release/Obj/sensorsim.d
@@ -0,0 +1,5 @@
+Output/ble_app_template_pca10056_s140\ Release/Obj/sensorsim.o: \
+ C:\Users\sho_o\labo\Nordic52SDK\nRF5SDK1702\nRF5_SDK_17.0.2\components\libraries\sensorsim\sensorsim.c \
+ C:\Users\sho_o\labo\Nordic52SDK\nRF5SDK1702\nRF5_SDK_17.0.2\components\libraries\sensorsim\sensorsim.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdbool.h
