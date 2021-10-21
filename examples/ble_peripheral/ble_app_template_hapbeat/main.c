f774ef74b12fd08c0441df728bd4d3fdb426bfcc
Onodera Sho <onodera@haselab.net>
2021-10-22 00:08:40 +0900

    delete nrf_drv_stop hapbeat Release -> Debug is good

--- examples/ble_peripheral/ble_app_template_hapbeat/main.c
+++ examples/ble_peripheral/ble_app_template_hapbeat/main.c
@@ -404,10 +404,10 @@ static void pwm_update(void)
             sum = ADPCMDecoder((code >> 4) & 0x0f, &state);
             sum = filter(sum);
             decode_flag = true;
-        } else {
-           nrf_drv_pwm_stop(&m_pwm0, false);
-           return;
-        }
+        } //else {
+           //nrf_drv_pwm_stop(&m_pwm0, false);
+           //return;
+        //}
     }
     /*
     if(!nrf_queue_is_empty(&m_byte_queue))
@@ -443,7 +443,7 @@ static void pwm_update(void)
     else if(value < 0) value = 0;
     p_channels[0] = value;
 
-    if(sum==0) nrf_drv_pwm_stop(&m_pwm0, false);
+    //if(sum==0) nrf_drv_pwm_stop(&m_pwm0, false);
     (void)nrf_drv_pwm_simple_playback(&m_pwm0, &m_seq, 1, NRF_DRV_PWM_FLAG_LOOP);
     return;
        
